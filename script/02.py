bee326b7ed1998d3cd54d7c09f673833c29da94b
Sushil Singh <36241112+OddExtension5@users.noreply.github.com>
2021-01-16 12:46:12 +0530

    Create 02.py

---
+++ script/02.py
@@ -0,0 +1,21 @@
+#################################################################
+#################### PROBLEM 02 #################################
+#################################################################
+
+
+"""
+
+A student has to travel a distance of 450 km by car at a certain average speed. Write a Python script to find the total time taken to cover the distance.
+
+""
+
+## SOLUTION
+
+Distance = 450
+Speed = eval(input("Enter average speed:"))  # to input speed from the user
+Time= Distance/Speed                         # calculate time taken
+print("Time Taken:", Time, "hr")
+
+
+
+
